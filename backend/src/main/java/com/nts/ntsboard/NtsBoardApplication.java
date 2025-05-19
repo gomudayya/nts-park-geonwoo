cea9a8b3c96eb7e7a555c474afe1f21a81264d0c
gomudayya <gomudayya@gmail.com>
2025-05-19 23:50:02 +0900

    chore : 스프링 프로젝트 생성

---
+++ backend/src/main/java/com/nts/ntsboard/NtsBoardApplication.java
@@ -0,0 +1,13 @@
+package com.nts.ntsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtsBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtsBoardApplication.class, args);
+    }
+
+}
