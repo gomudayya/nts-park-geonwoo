8ed8311278670b638efab2a422a92e7fddea7f11
gomudayya <gomudayya@gmail.com>
2025-05-21 01:40:27 +0900

    fix : cors 설정 추가

---
+++ backend/src/main/java/com/nts/ntsboard/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.nts.ntsboard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true) // 쿠키 허용
+                .maxAge(3600);// preflight 캐시 시간 (초)
+    }
+}
