161732df924d92b49b233f42ac5f5952c83d14be
gomudayya <gomudayya@gmail.com>
2025-05-26 23:58:18 +0900

    Docker-compose 실행방식 추가

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,7 +3,7 @@ import { setInterceptors } from '@/api/interceptors';
 
 function createApiInstance() {
   const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://127.0.0.1:8080',
   });
 
   return setInterceptors(instance);
